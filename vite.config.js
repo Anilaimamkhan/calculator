d104bc4f2cc468522466c265b112db7b094151d5
Anilaimamkhan <anilaimam19@gmail.com>
2025-07-14 12:38:40 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()]
+  
+// })
+
+
+
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Check environment variable
+const isGitHubPages = process.env.DEPLOY_TARGET === 'GH_PAGES';
+
 export default defineConfig({
-  plugins: [react()]
-  
+  plugins: [react()],
+  base: isGitHubPages ? '/calculator/' : '/', // ✅ dynamic base
 })
+
